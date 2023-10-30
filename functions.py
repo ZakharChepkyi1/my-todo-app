a84358371ee4c30a37a280bf021fc75b980fd1df
Zakhar <zakhar.chepkyi.2020@gmail.com>
2023-10-30 22:59:17 +0100

    initial commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+FILEPATH = "todos.txt"
+def get_todo(filepath = FILEPATH):
+    """" Read a text file  ard return the  list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    """ Write the to-do list in the text file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
